d90a0bd88e5f7c35b9dd429d988ade538331bb23
Honghao Li <hli@greenactionstudio.com>
2021-12-17 13:20:28 -0500

    working demo 0.2.6

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.post('/test', async (req, res) => {
     res.send(req.body)
 })
 
+//retrieve user strategy by id
 app.get('/getUserStrategy/:userId', async (req, res) => {
     const id = req.params.userId;
     (async () => {
@@ -46,6 +47,7 @@ app.get('/getUserStrategy/:userId', async (req, res) => {
     })().catch(e => res.send(e))
 })
 
+//update user strategy
 app.post('/updateUserStrategy/:userId/:strategy', async (req, res) => {
     const id = req.params.userId;
     const strategy = req.params.strategy;
@@ -69,16 +71,19 @@ app.post('/updateUserStrategy/:userId/:strategy', async (req, res) => {
     })().catch(e => res.send(e))
 })
 
+//register a new user on master server
 app.post('/registerUser/:userId', async (req, res) => {
     const id = req.params.userId;
     (async () => {
         const client = await pool.connect()
         try {
             await client.query('BEGIN')
+            //check if user already exists
             let q0 = `select *
                       from users
                       where id = $1`
             let result = await client.query(q0, [id])
+            //if user not existed, create user and default user strategy
             if (result.rowCount == 0) {
                 let q1 = `insert into users
                           values ($1)`
@@ -89,10 +94,9 @@ app.post('/registerUser/:userId', async (req, res) => {
                 await client.query('COMMIT')
                 res.send(id + ' registered successfully and default strategy set as fcfs')
             } else {
+                //if user exists, do nothing
                 res.send('user already existed')
-
             }
-
         } catch (e) {
             await client.query('ROLLBACK')
             throw e
@@ -102,11 +106,12 @@ app.post('/registerUser/:userId', async (req, res) => {
     })().catch(e => res.send(e))
 })
 
-
+//for application instance to publish task to master
 app.post('/addTask', async (req, res) => {
     (async () => {
         const client = await pool.connect()
         try {
+            //insert the task to taskQueue
             await client.query('BEGIN');
             const uid = uuidv4();
             const task = req.body.query;
@@ -115,22 +120,26 @@ app.post('/addTask', async (req, res) => {
             const queryText = `insert into taskQueue
                                values ($1, $2, $3, $4)`
             await client.query(queryText, [uid, userId, address, task])
-            //if there are nodes on, then perform changes depending on strategy
+            //if there are sessions on, then perform changes depending on strategy
             let result = await client.query(`select *
                                              from session
                                              where userId = $1
                                                and applicationAddress != $2
                                                and status = 'on'`, [userId, address])
             const sessions = result.rows;
+            //if no other session online, commit the changes to taskQueue
             if (sessions.length === 0) {
                 await client.query('COMMIT')
             } else {
+                //if there are other session online, force them to adapt the changes and do not commit task to taskQueue
                 result = await client.query(`select *
                                              from userStrategy
                                              where userId = $1`, [userId])
                 const strategy = result.rows[0].strategyname;
+
+                //for every session
                 for (let i = 0; i < sessions.length; i++) {
-                    //    send to each server and force execute
+                    //force execute
                     axios
                         .post(sessions[i].applicationaddress + '/run', {
                             todo: task,
@@ -143,8 +152,9 @@ app.post('/addTask', async (req, res) => {
                             console.error(error)
                         })
                 }
-                await client.query('ROLLBACK')
 
+                //no changes to commit
+                await client.query('ROLLBACK')
             }
             res.send({status: 'ok'})
         } catch (e) {
@@ -156,9 +166,11 @@ app.post('/addTask', async (req, res) => {
     })().catch(e => res.send(e))
 })
 
+//get all session by userId and app address
 app.get('/getSession/:userId/:address', async (req, res) => {
     const id = req.params.userId;
     const address = "http://localhost:" + req.params.address;
+
     (async () => {
         const client = await pool.connect()
         try {
@@ -179,9 +191,7 @@ app.get('/getSession/:userId/:address', async (req, res) => {
     })().catch(e => res.send(e))
 })
 
-
-
-
+//get task from master by userId and application address
 app.get('/getTask/:userId/:address', async (req, res) => {
     const id = req.params.userId;
     const address = "http://localhost:" + req.params.address;
@@ -205,6 +215,7 @@ app.get('/getTask/:userId/:address', async (req, res) => {
     })().catch(e => res.send(e))
 })
 
+//notify master some tasks are done and should be removed from taskQueue
 app.post('/deleteTask', async (req, res) => {
     const ids = req.body['ids'];
     (async () => {
@@ -229,19 +240,21 @@ app.post('/deleteTask', async (req, res) => {
     })().catch(e => res.send(e))
 })
 
-
+//update the current status of the session
 app.post('/updateSession', async (req, res) => {
     const rb = req.body;
     (async () => {
         const client = await pool.connect()
         try {
             await client.query('BEGIN')
+            //check if session with the same userId and App address exists before
             let query = `select *
                          from session
                          where userId = $1
                            and applicationAddress = $2`
             let result = await client.query(query, [rb.userId, rb.applicationAddress])
             if (result.rowCount == 0) {
+                //if never existed before, record application address and create session
                 query = `insert into userApplicationAddress
                          values ($1, $2)`
                 result = await client.query(query, [rb.userId, rb.applicationAddress])
@@ -249,6 +262,7 @@ app.post('/updateSession', async (req, res) => {
                          values ($1, $2, $3)`
                 result = await client.query(query, [rb.userId, rb.applicationAddress, 'on'])
             } else {
+                //if existed before, just perform update (on/off)
                 query = `update session
                          set status = $3
                          where userId = $1
